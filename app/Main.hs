752596aece6710290e117430b1895a07499c903e
Philip Kamenarsky <p.kamenarsky@gmail.com>
2019-12-05 21:17:43 +0000

    Tests

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,35 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Network.WebSockets.Connection
-
 import qualified Connector.WebSocket as WS
+
 import qualified Connector.Log as Log
+import qualified Connector.HTTP as HTTP
 
 import Concur
 
+import Network.HTTP.Types.Status
+import Network.WebSockets.Connection
+import Network.Wai
+
 test :: IO ()
 test = do
+  HTTP.http 3921 $ \http -> do
   WS.websocket 3922 defaultConnectionOptions $ \wss -> do
   WS.websocket 3923 defaultConnectionOptions $ \wss2 -> do
   Log.logger $ \log -> do
-    runConcur $ server' log wss wss2
+
+    runConcur $ auth http log wss wss2
 
     where
+      auth http log wss wss2 = do
+        r <- HTTP.receive http $ \req respond -> do
+          r <- respond $ responseLBS status200 [] "Hello World"
+          pure (r, "good")
+        log r
+        server' log wss wss2
+      
       server' log wss wss2
         = loopOrr [ Left <$> andd [ WS.accept wss, WS.accept wss2 ] ] $ \r -> do
             case r of
