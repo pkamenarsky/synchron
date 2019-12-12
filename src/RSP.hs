aac147b0111f9ade9cec58fac9f073fa6e03dfb1
Philip Kamenarsky <p.kamenarsky@gmail.com>
2019-12-12 12:32:25 +0000

    RSP WIP

--- src/RSP.hs
+++ src/RSP.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TupleSections #-}
 
 module RSP where
 
@@ -14,21 +15,28 @@ import Unsafe.Coerce (unsafeCoerce)
 
 newtype Context = Context (MVar (Maybe (RSP ())))
 
-newtype Event a = Event (IORef a)
+newtype Event a = Event (IORef (Maybe a))
 
 newEvent :: IO (Event a)
-newEvent = Event <$> newIORef undefined
+newEvent = Event <$> newIORef Nothing
 
 emit :: Context -> Event a -> a -> IO Bool
 emit (Context ctx) e@(Event event) a = do
-  writeIORef event a
-  modifyMVar ctx $ \rsp' -> case rsp' of
-    Nothing -> pure (Nothing, False)
-    Just rsp -> do
-      rsp' <- runRSP e rsp
-      case rsp' of
-        Left _ -> pure (Nothing, True)
-        Right rsp' -> pure (Just rsp', True)
+  modifyMVar ctx $ \rsp' -> do
+    writeIORef event (Just a)
+    case rsp' of
+      Nothing -> pure (Nothing, False)
+      Just rsp -> (,True) <$> go e rsp
+  where
+    go e rsp = do
+      a <- runRSP e rsp
+      writeIORef event Nothing
+      case a of
+        Left _ -> pure Nothing
+        (Right (Left rsp')) -> do
+          pure (Just rsp')
+        (Right (Right rsp')) -> do
+          go e rsp'
 
 --------------------------------------------------------------------------------
 
@@ -60,7 +68,7 @@ andd rsps = RSP $ liftF (And rsps id)
 sameReference :: IORef a -> IORef b -> Bool
 sameReference = unsafeCoerce ((==) :: IORef a -> IORef a -> Bool)
 
-runRSP :: Event b -> RSP a -> IO (Either a (RSP a))
+runRSP :: Event b -> RSP a -> IO (Either a (Either (RSP a) (RSP a)))
 runRSP _ (RSP (Pure a)) = pure (Left a)
 runRSP e (RSP (Free (Step io next))) = do
   a <- io
@@ -69,28 +77,43 @@ runRSP (Event currentEvent) rsp@(RSP (Free (Await (Event event) next))) = do
   if sameReference currentEvent event
     then do
       a <- readIORef event
-      pure (Right $ RSP (next a))
+      case a of
+        Just a' -> pure (Right $ Right $ RSP (next a'))
+        Nothing -> pure (Right $ Left rsp)
     else
-      pure (Right rsp)
+      pure (Right $ Left rsp)
 runRSP e (RSP (Free (Or rsps next))) = do
   as <- zip rsps <$> traverse (runRSP e) rsps
   go Nothing [] as
   where
-    go (Just a) rs [] = pure (Right $ RSP $ next (a, rs))
-    go Nothing rs []  = pure (Right $ RSP $ Free $ Or rs next)
+    go (Just a) rs [] = pure (Right $ Right $ RSP $ next (a, rs))
+
+    -- TODO: is this blocked? infinite loop?
+    go Nothing rs []  = pure (Right $ Left $ RSP $ Free $ Or rs next)
 
     go (Just a) rs ((rsp, _):xs) = go (Just a) (rsp:rs) xs
 
     go Nothing rs (((_, Left a):xs)) = go (Just a) rs xs
-    go Nothing rs (((_, Right rsp'):xs)) = go Nothing (rsp':rs) xs
+    go Nothing rs (((_, Right (Left rsp')):xs)) = go Nothing (rsp':rs) xs
+    go Nothing rs (((_, Right (Right rsp')):xs)) = go Nothing (rsp':rs) xs
 runRSP e (RSP (Free (And rsps next))) = do
   as <- traverse (runRSP e) rsps
-  case (traverse done as, sequence as) of
-    (Just as', _)    -> pure (Right $ RSP $ next as')
-    (_, Right rsps') -> pure (Right $ RSP $ Free $ And rsps' next)
+  case traverse done as of
+    Just as' -> pure (Right $ Right $ RSP $ next as')
+    Nothing -> if any isBlocked as
+      then pure (Right $ Left $ RSP $ Free $ And (map collapse as) next)
+      else pure (Right $ Right $ RSP $ Free $ And (map collapse as) next)
   where
     done (Left a) = Just a
     done _ = Nothing
+
+    isBlocked (Left _) = False
+    isBlocked (Right (Left _)) = True
+    isBlocked (Right (Right _)) = False
+
+    collapse (Left a) = RSP (Pure a)
+    collapse (Right (Left a)) = a
+    collapse (Right (Right b)) = b
 
 run :: RSP () -> IO Context
 run rsp = Context <$> newMVar (Just rsp)
@@ -107,8 +130,11 @@ server app = do
   e <- newEvent
   ctx <- app e
   emit ctx e "a"
+  emit ctx e "b"
   pure ctx
 
 m = server $ \x -> server $ \y -> server $ \z -> run $ do
-  a <- await x
+  a <- andd [ await x, await y, await z ]
+  step $ print a
+  a <- andd [ await x, await y, await z ]
   step $ print a
