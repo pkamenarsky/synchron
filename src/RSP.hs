658d1d0ce7e6c82da6e665da8aa5a3d8f9fe76c7
Philip Kamenarsky <p.kamenarsky@gmail.com>
2019-12-18 01:40:02 +0000

    WIP

--- src/RSP.hs
+++ src/RSP.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TupleSections #-}
 
@@ -10,6 +12,7 @@ import Control.Concurrent
 import Control.Monad.Free
 
 import Data.IORef
+import Data.Maybe (isJust)
 
 import Unsafe.Coerce (unsafeCoerce)
 
@@ -29,22 +32,33 @@ emit (Context ctx) e@(Event event) a = do
     writeIORef event (Just a)
     case rsp' of
       Nothing -> pure (Nothing, False)
-      Just rsp -> (,True) <$> go e rsp
+      Just rsp -> (,True) <$> go [] (Right e) rsp
   where
-    go e rsp = do
+    go ks e rsp = do
       a <- runRSP e rsp
       writeIORef event Nothing
       case a of
-        Left _ -> pure Nothing
-        (Right (Left rsp')) -> do
-          pure (Just rsp')
-        (Right (Right rsp')) -> do
-          go e rsp'
+        Done _ -> pure Nothing
+        Blocked rsp' -> case ks of
+          [] -> pure (Just rsp')
+          ((e', k):ks) -> go ks e' (k rsp)
+        Next rsp' -> do
+          go ks e rsp'
+        Cont e' b k -> go ((e, k):ks) (Left (VEvent e' b)) rsp
 
 --------------------------------------------------------------------------------
 
+data IEvent a = IEvent Int
+
+data VEvent = forall a. VEvent (IEvent a) a
+
 data RSPF next
   = Async (IO ()) next
+
+  | forall a. Local (IEvent a -> RSP ()) next
+  | forall a. EmitI (IEvent a) a next
+  | forall a. AwaitI (IEvent a) (a -> next)
+
   | forall a. Await (Event a) (a -> next)
   | forall a. Or [RSP a] ((a, [RSP a]) -> next)
   | forall a. And [RSP a] ([a] -> next)
@@ -56,6 +70,15 @@ newtype RSP a = RSP { getRSP :: Free RSPF a }
 
 async :: IO () -> RSP ()
 async io = RSP $ liftF (Async io ())
+
+local :: (IEvent a -> RSP ()) -> RSP ()
+local f = RSP $ liftF (Local f ())
+
+emitI :: IEvent a -> a -> RSP ()
+emitI e a = RSP $ liftF (EmitI e a ())
+
+awaitI :: IEvent a -> RSP a
+awaitI e = RSP $ liftF (AwaitI e id)
 
 await :: Event a -> RSP a
 await e = RSP $ liftF (Await e id)
@@ -71,52 +94,99 @@ andd rsps = RSP $ liftF (And rsps id)
 sameReference :: IORef a -> IORef b -> Bool
 sameReference = unsafeCoerce ((==) :: IORef a -> IORef a -> Bool)
 
-runRSP :: Event b -> RSP a -> IO (Either a (Either (RSP a) (RSP a)))
-runRSP _ (RSP (Pure a)) = pure (Left a)
+data Run a
+  = Done a
+  | Blocked (RSP a)
+  | Next (RSP a)
+  | forall b. Cont (IEvent b) b (RSP a -> RSP a)
+
+data Focus a = Focus a [a] [a]
+
+focus :: (a -> Maybe b) -> [a] -> Maybe (b, Focus a)
+focus f as = case break (isJust . f) as of
+  (xs, y:ys) -> (,) <$> f y <*> Just (Focus y xs ys)
+  (_, [])    -> Nothing
+
+get :: Focus a -> a
+get (Focus a _ _) = a
+
+without :: Focus a -> [a]
+without (Focus _ xs ys) = xs <> ys
+
+replace :: Focus a -> (b -> b) -> [b] -> [b]
+replace (Focus a xs _) f bs
+  = take (length xs) bs <> [f (head $ drop l bs)] <> drop (l + 1) bs
+  where
+    l = length xs
+
+isDone :: Run a -> Maybe a
+isDone (Done a) = Just a
+isDone _ = Nothing
+
+isCont :: Run a -> Maybe (Run a)
+isCont rsp@(Cont _ _ _) = Just rsp
+isCont _ = Nothing
+
+runRSP :: Either VEvent (Event b) -> RSP a -> IO (Run a)
+runRSP _ (RSP (Pure a)) = pure (Done a)
 runRSP e (RSP (Free (Async io next))) = do
   forkIO io
   runRSP e (RSP next)
-runRSP (Event currentEvent) rsp@(RSP (Free (Await (Event event) next))) = do
+
+-- Await
+runRSP (Right (Event currentEvent)) rsp@(RSP (Free (Await (Event event) next))) = do
   if sameReference currentEvent event
     then do
       a <- readIORef event
       case a of
-        Just a' -> pure (Right $ Right $ RSP (next a'))
-        Nothing -> pure (Right $ Left rsp)
+        Just a' -> pure (Next $ RSP (next a'))
+        Nothing -> pure (Blocked rsp)
     else
-      pure (Right $ Left rsp)
+      pure (Blocked rsp)
+runRSP _ rsp@(RSP (Free (Await _ _))) = pure (Blocked rsp)
+
+-- IAwait
+runRSP (Left (VEvent (IEvent e) a)) rsp@(RSP (Free (AwaitI (IEvent event) next))) = do
+  if e == event
+    then pure (Next $ RSP (next $ unsafeCoerce a))
+    else pure (Blocked rsp)
+runRSP _ rsp@(RSP (Free (AwaitI _ _))) = pure (Blocked rsp)
+
+runRSP _ (RSP (Free (EmitI e a next))) = do
+  pure $ Cont e a $ \_ -> RSP next
+
+-- Or
 runRSP e (RSP (Free (Or rsps next))) = do
-  as <- zip rsps <$> traverse (runRSP e) rsps
-  go Nothing [] as
-  where
-    go (Just a) rs [] = pure (Right $ Right $ RSP $ next (a, rs))
+  as <- traverse (runRSP e) rsps
 
-    -- TODO: is this blocked? infinite loop?
-    go Nothing rs []  = pure (Right $ Left $ RSP $ Free $ Or rs next)
+  case focus isDone as of
+    Just (a, z) -> pure (Next $ RSP $ next (a, map collapse (without z)))
+    Nothing -> case focus isCont as of
+      Just (Cont e b f, z) -> pure $ Cont e b $ \rsp -> case rsp of
+        RSP (Free (Or rsps' next')) -> RSP $ Free (Or (replace z (unsafeCoerce f) rsps') next')
+        rsp' -> rsp'
+      _ -> pure (Blocked $ RSP $ Free $ Or (map collapse as) next)
 
-    go (Just a) rs ((rsp, _):xs) = go (Just a) (rsp:rs) xs
-
-    go Nothing rs (((_, Left a):xs)) = go (Just a) rs xs
-    go Nothing rs (((_, Right (Left rsp')):xs)) = go Nothing (rsp':rs) xs
-    go Nothing rs (((_, Right (Right rsp')):xs)) = go Nothing (rsp':rs) xs
+-- And
 runRSP e (RSP (Free (And rsps next))) = do
   as <- traverse (runRSP e) rsps
-  case traverse done as of
-    Just as' -> pure (Right $ Right $ RSP $ next as')
-    Nothing -> if any isBlocked as
-      then pure (Right $ Left $ RSP $ Free $ And (map collapse as) next)
-      else pure (Right $ Right $ RSP $ Free $ And (map collapse as) next)
+  case traverse isDone as of
+    Just as' -> pure (Next $ RSP $ next as')
+    Nothing -> case focus isCont as of
+      Just (Cont e b f, z) -> pure $ Cont e b $ \rsp -> case rsp of
+        RSP (Free (Or rsps' next')) -> RSP $ Free (Or (replace z (unsafeCoerce f) rsps') next')
+        rsp' -> error "Cont: And"
+      _ -> if any isBlocked as
+        then pure (Blocked $ RSP $ Free $ And (map collapse as) next)
+        else pure (Next $ RSP $ Free $ And (map collapse as) next)
   where
-    done (Left a) = Just a
-    done _ = Nothing
+    isBlocked (Done _) = False
+    isBlocked (Blocked _) = True
+    isBlocked (Next _) = False
 
-    isBlocked (Left _) = False
-    isBlocked (Right (Left _)) = True
-    isBlocked (Right (Right _)) = False
-
-    collapse (Left a) = RSP (Pure a)
-    collapse (Right (Left a)) = a
-    collapse (Right (Right b)) = b
+collapse (Done a) = RSP (Pure a)
+collapse (Blocked a) = a
+collapse (Next b) = b
 
 run :: RSP () -> IO Context
 run rsp = Context <$> newMVar (Just rsp)
