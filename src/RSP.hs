487fb1c41ff7f7c7e6b103252d004e7417811ea9
Philip Kamenarsky <p.kamenarsky@gmail.com>
2019-12-20 16:51:27 +0200

    WIP

--- src/RSP.hs
+++ src/RSP.hs
@@ -12,7 +12,7 @@ import Control.Concurrent
 import Control.Monad.Free
 
 import Data.IORef
-import Data.Maybe (isJust)
+import Data.Either (isLeft)
 
 import System.IO.Unsafe (unsafePerformIO)
 import Unsafe.Coerce (unsafeCoerce)
@@ -21,7 +21,9 @@ import Debug.Trace
 
 --------------------------------------------------------------------------------
 
-data Event a = Event [Int]
+type EventId = Int
+
+data Event a = Event EventId
 
 data RSPF next
   = Async (IO ()) next
@@ -63,46 +65,30 @@ forever = RSP $ liftF Forever
 
 --------------------------------------------------------------------------------
 
-data Run a
-  = Done a
-  | Blocked (RSP a)
-  | Next (RSP a)
-  | forall b. Cont (Event b) b (RSP a -> RSP a)
-
-isDone :: Run a -> Maybe a
-isDone (Done a) = Just a
-isDone _ = Nothing
-
-isCont :: Run a -> Maybe (Run a)
-isCont rsp@(Cont _ _ _) = Just rsp
-isCont _ = Nothing
-
---------------------------------------------------------------------------------
-
 data Focus a = Focus a [a] [a]
+  deriving Functor
 
-focus :: (a -> Maybe b) -> [a] -> Maybe (b, Focus a)
-focus f as = case break (isJust . f) as of
-  (xs, y:ys) -> (,) <$> f y <*> Just (Focus y xs ys)
-  (_, [])    -> Nothing
+focus :: (a -> Either b c) -> [a] -> Either (b, Focus a) [c]
+focus f as = case break (isLeft . f) as of
+  (xs, y:ys) -> case f y of
+    Left y' -> Left (y', Focus y xs ys)
+    _       -> undefined
+  (_, []) -> undefined
 
 get :: Focus a -> a
 get (Focus a _ _) = a
 
-index :: Focus a -> Int
-index = undefined
-
 without :: Focus a -> [a]
 without (Focus _ xs ys) = xs <> ys
 
-replace :: Focus a -> (b -> b) -> [b] -> [b]
-replace (Focus _ xs _) f bs
+modify :: Focus a -> (b -> b) -> [b] -> [b]
+modify (Focus _ xs _) f bs
   = take (length xs) bs <> [f (head $ drop l bs)] <> drop (l + 1) bs
   where
     l = length xs
 
-replace' :: Focus a -> b -> [b] -> [b]
-replace' (Focus _ xs _) a bs
+replace :: Focus a -> b -> [b] -> [b]
+replace (Focus _ xs _) a bs
   = take (length xs) bs <> [a] <> drop (l + 1) bs
   where
     l = length xs
@@ -115,107 +101,81 @@ data R a
   = D a
   | B (RSP a)
   | C (K a)
-  -- | A (IO ()) (RSP a)
 
-anyDone :: [R a] -> Either (a, [RSP a]) [RSP a]
-anyDone = undefined
+isDone :: R a -> Either a (R a)
+isDone (D a) = Left a
+isDone r     = Right r
 
-anyCont :: [R a] -> Maybe (K a, Focus (RSP a))
-anyCont = undefined
+anyDone :: [(R a, RSP a)] -> Either (a, Focus (RSP a)) [R a]
+anyDone rs = case focus (isDone . fst) rs of
+  Left (a, z) -> Left (a, snd <$> z)
+  Right rbcs  -> Right rbcs
 
-allDone :: [R a] -> Either [a] [RSP a]
-allDone = undefined
+allDone :: [R a] -> Either [a] [R a]
+allDone rs = case traverse (swap . isDone) rs of
+  Right as -> Left as
+  Left _   -> Right rs
+  where
+    swap (Left a)  = Right a
+    swap (Right b) = Left b
 
-blocked :: [R a] -> Maybe [RSP a]
-blocked = undefined
-
--- completeRSP :: [Int] -> Maybe (Event b, b) -> RSP a -> R a
--- completeRSP _ _ (RSP (Pure a)) = D a
--- completeRSP i _ rsp@(RSP (Free Forever)) = B rsp
--- -- completeRSP _ _ (RSP (Free (Async io next))) = A io (RSP next)
--- completeRSP i@(n:ns) e (RSP (Free (Local f next))) =
---   completeRSP (n + 1:ns) e (f (Event i) >> RSP next)
--- completeRSP i@(n:ns) (Just (Event e, v)) rsp@(RSP (Free (Await (Event e') next))) =
---   if e == e'
---     then completeRSP (n + 1:ns) Nothing (RSP $ next $ unsafeCoerce v)
---     else B rsp
--- completeRSP i@(n:ns) _ (RSP (Free (Emit e v _))) =
---   -- Set a temporary 'forever' placeholder so that the emit isn't reevaluated
---   C $ K e v forever $ \i' rsp -> case rsp of
---     RSP (Free (Emit _ _ next))
---       | i == i' -> completeRSP (n + 1:ns) Nothing (RSP next)
---     _ -> B rsp
--- completeRSP i@(n:ns) e (RSP (Free (Or rsps next))) =
---   case anyDone rs of
---   -- 1. If a trail is done, next
---       Left (a, rsps') -> completeRSP (n + 1:ns) Nothing (RSP $ next (a, rsps'))
---       Right rsps'     -> case anyCont rs of
---   -- 2. If a trail is a continuation, return a continuation
---         Just (k, z) -> resume rsps' k z
---   -- 3. If not a continuation, return blocked state
---         Nothing     -> B $ RSP $ Free $ Or rsps' next
--- 
---   where
---     rs = map (\(m, rsp) -> completeRSP (0:m:n:ns) e rsp) (zip [0..] rsps)
--- 
---     resume rsps' (K e v h k) z = C $ K e v (RSP $ Free $ Or (replace' z h rsps') next) $ \i' rsp -> case rsp of
---        -- 2.1 Compare constructor and previous step id with current step id
---        RSP (Free (Or rsps'' next'))
---        -- 2.1.1 If there's match, continue with same id and no event
---          | i == i' -> case k (0:index z:n:ns) (get z) of
---              D a         -> completeRSP i' Nothing (RSP $ Free $ Or (replace' z (RSP $ Pure $ unsafeCoerce a) rsps'') next')
---              B rsp       -> completeRSP i' Nothing (RSP $ Free $ Or (replace' z (unsafeCoerce rsp) rsps'') next')
---              C k         -> resume rsps' k z
---              -- A io next'' -> A io (RSP $ Free $ Or (replace' z (unsafeCoerce next'') rsps'') next')
---        -- 2.1.2 Otherwise, return unchanged state
---        _ -> B rsp
--- completeRSP i@(n:ns) e (RSP (Free (And rsps next))) =
---   case allDone rs of
---     Left as     -> completeRSP (n + 1:ns) Nothing (RSP $ next as)
---     Right rsps' -> B $ RSP $ Free $ And rsps' next
--- 
---   where
---     rs = map (\(m, rsp) -> completeRSP (0:m:n:ns) e rsp) (zip [0..] rsps)
+anyCont :: [(R a, RSP a)] -> Either (K a, Focus (RSP a)) [RSP a]
+anyCont rs = case focus isCont rs of
+  Left (k, z) -> Left (k, snd <$> z)
+  Right rsps  -> Right rsps
+  where
+    isCont (C k, _)   = Left k
+    isCont (B rsp, _) = Right rsp
+    isCont (D a, _)   = Right (RSP $ Pure a)
 
 -- This doesn't change structure
 reactRSP :: Event b -> b -> RSP a -> RSP a
+-- Await
 reactRSP (Event e) a rsp@(RSP (Free (Await (Event e') next))) = if e == e'
   then RSP $ next $ unsafeCoerce a
   else rsp
-reactRSP event@(Event e) a rsp@(RSP (Free (Or rsps next))) = RSP $ Free $ Or (map (reactRSP event a) rsps) next
-reactRSP event@(Event e) a rsp@(RSP (Free (And rsps next))) = RSP $ Free $ And (map (reactRSP event a) rsps) next
+-- Or
+reactRSP event@(Event e) a rsp@(RSP (Free (Or rsps next)))
+  = RSP $ Free $ Or (map (reactRSP event a) rsps) next
+-- And
+reactRSP event@(Event e) a rsp@(RSP (Free (And rsps next)))
+  = RSP $ Free $ And (map (reactRSP event a) rsps) next
+-- _
 reactRSP _ _ rsp = rsp
 
 advanceRSP :: RSP a -> R a
-advanceRSP (RSP (Pure a)) = D a
-advanceRSP rsp@(RSP (Free Forever)) = B rsp
-advanceRSP (RSP (Free (Local f next))) = advanceRSP (f (Event undefined) >> RSP next)
-advanceRSP rsp@(RSP (Free (Await _ _))) = B rsp
-advanceRSP (RSP (Free (Emit e v _))) = C $ K e v $ \rsp -> undefined
+-- Pure
+advanceRSP (RSP (Pure a))                = D a
+-- Forever
+advanceRSP rsp@(RSP (Free Forever))      = B rsp
+-- Local
+advanceRSP (RSP (Free (Local f next)))   = advanceRSP (f (Event undefined) >> RSP next)
+-- Await
+advanceRSP rsp@(RSP (Free (Await _ _)))  = B rsp
+-- Emit
+advanceRSP (RSP (Free (Emit e v next)))  = C $ K e v $ \_ -> advanceRSP (RSP next)
+-- Or
 advanceRSP (RSP (Free (Or rsps next))) =
-  case anyDone rs of
-      Left (a, rsps') -> advanceRSP (RSP $ next (a, rsps'))
-      Right rsps'     -> case anyCont rs of
-        Just (k, z) -> resume rsps' k z
-        Nothing     -> B $ RSP $ Free $ Or rsps' next
-
-  where
-    rs = map advanceRSP rsps
-
-    resume rsps' (K e v k) z = C $ K e v $ \rsp -> case rsp of
-       RSP (Free (Or rsps'' next')) -> case k (get z) of
-             D a         -> advanceRSP (RSP $ Free $ Or (replace' z (RSP $ Pure $ unsafeCoerce a) rsps'') next')
-             B rsp       -> advanceRSP (RSP $ Free $ Or (replace' z (unsafeCoerce rsp) rsps'') next')
-             C k         -> resume rsps' k z
-       _ -> B rsp
-
+  case anyDone (zip (map advanceRSP rsps) rsps) of
+      Left (a, z) -> advanceRSP (RSP $ next (a, without z))
+      Right rbcs  -> case anyCont (zip rbcs rsps) of
+        Left (k, z) -> resume rsps k z
+        Right rsps' -> B $ RSP $ Free $ Or rsps' next
+-- And
 advanceRSP (RSP (Free (And rsps next))) =
-  case allDone rs of
-    Left as     -> advanceRSP (RSP $ next as)
-    Right rsps' -> B $ RSP $ Free $ And rsps' next
+  case allDone (map advanceRSP rsps) of
+    Left as    -> advanceRSP (RSP $ next as)
+    Right rbcs -> case anyCont (zip rbcs rsps) of
+      Left (k, z) -> resume rsps k z
+      Right rsps' -> B $ RSP $ Free $ And rsps' next
 
-  where
-    rs = map (\(m, rsp) -> advanceRSP rsp) (zip [0..] rsps)
+resume :: [RSP a] -> K a -> Focus (RSP a) -> R b
+resume rsps (K e v k) z = C $ K e v $ \rsp -> case rsp of
+   RSP (Free (Or rsps' next')) -> case k (get z) of
+         D a         -> advanceRSP (RSP $ Free $ Or (replace z (RSP $ Pure $ unsafeCoerce a) rsps') next')
+         B rsp       -> advanceRSP (RSP $ Free $ Or (replace z (unsafeCoerce rsp) rsps') next')
+         C k         -> resume rsps k z
+   _ -> error "advanceRSP: Or"
 
 -- runR :: [Int] -> [([Int], [Int] -> RSP a -> R a)] -> R a -> IO a
 -- runR _ _ (D a) = pure a
