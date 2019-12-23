55fd63696d596deefef2bb36ec62aef9a3adf98b
Philip Kamenarsky <p.kamenarsky@gmail.com>
2019-12-23 21:58:05 +0200

    Local events/tests

--- src/RSP.hs
+++ src/RSP.hs
@@ -14,6 +14,7 @@ import Control.Monad.IO.Class
 import qualified Control.Monad.Trans.State as ST
 import qualified Control.Lens as L
 
+import Data.Bifunctor (second)
 import Data.IORef
 import Data.List (intercalate)
 import Data.Maybe (listToMaybe, mapMaybe)
@@ -24,11 +25,21 @@ import Debug.Trace
 
 --------------------------------------------------------------------------------
 
-type EventId = IORef ()
+newtype EventIdInt = EventIdInt Int
+  deriving (Num, Eq, Ord, Show)
+
+newtype EventIdRef = EventIdRef (IORef ())
+  deriving Eq
+
+data EventId = I EventIdInt | R EventIdRef
+  deriving Eq
 
 data Event a = Event EventId
 
-type IsHole = Bool
+data ExEvent = forall a. ExEvent (Event a) a
+
+newtype StackLevel = StackLevel Int
+  deriving (Eq, Ord, Num, Show)
 
 data RSPF next
   = Async (IO ()) next
@@ -39,13 +50,13 @@ data RSPF next
   | Emit ExEvent next
   | forall a. Await (Event a) (a -> next)
 
-  | forall a. Or (RSP a) (RSP a) (a -> next)
+  | forall a. Or (RSP a) (RSP a) ((a, RSP a) -> next)
   | forall a. And (RSP a) (RSP a) ([a] -> next)
 
-  | forall a. OrU (RSP a) (RSP a) (a -> next)
+  | forall a. OrU (RSP a) (RSP a) ((a, RSP a) -> next)
   | forall a. AndU (RSP a) (RSP a) ([a] -> next)
 
-  | forall a. Canrun Int next
+  | forall a. Canrun StackLevel next
 
 deriving instance Functor RSPF
 
@@ -53,25 +64,31 @@ newtype RSP a = RSP { getRSP :: Free RSPF a }
   deriving (Functor, Applicative, Monad)
 
 instance Show (RSP a) where
-  show (RSP (Pure a)) = "Pure"
-  show (RSP (Free (Async _ _))) = "Async"
-  show (RSP (Free Forever)) = "Forever"
-  show (RSP (Free (Local _ _))) = "Local"
-  show (RSP (Free (Emit _ _))) = "Emit"
-  show (RSP (Free (Await _ _))) = "Await"
-  show (RSP (Free (Or a b _))) = "Or [" <> intercalate " " (map show [a, b]) <> "]"
-  show (RSP (Free (And a b _))) = "And [" <> intercalate " " (map show [a, b]) <> "]"
-  show (RSP (Free (OrU a b _))) = "OrU [" <> intercalate " " (map show [a, b]) <> "]"
-  show (RSP (Free (AndU a b _))) = "AndU [" <> intercalate " " (map show [a, b]) <> "]"
-  show (RSP (Free (Canrun n p))) = "Canrun " <> show n <> " (" <> show (RSP p) <> ")"
+  show (RSP (Pure a))            = "Pure"
+  show (RSP (Free (Async _ _)))  = "Async"
+  show (RSP (Free Forever))      = "Forever"
+  show (RSP (Free (Local _ _)))  = "Local"
+  show (RSP (Free (Emit _ _)))   = "Emit"
+  show (RSP (Free (Await _ _)))  = "Await"
+  show (RSP (Free (Or a b _)))   = "Or [" <> intercalate ", " (map show [a, b]) <> "]"
+  show (RSP (Free (And a b _)))  = "And [" <> intercalate ", " (map show [a, b]) <> "]"
+  show (RSP (Free (OrU a b _)))  = "OrU [" <> intercalate ", " (map show [a, b]) <> "]"
+  show (RSP (Free (AndU a b _))) = "AndU [" <> intercalate ", " (map show [a, b]) <> "]"
+  show (RSP (Free (Canrun (StackLevel n) p))) = "Canrun " <> show n <> " (" <> show (RSP p) <> ")"
 
 async :: IO () -> RSP ()
 async io = RSP $ liftF (Async io ())
 
-local :: (Event a -> IO b) -> IO b
-local f = do
+forever :: RSP a
+forever = RSP $ liftF Forever
+
+global :: (Event a -> IO b) -> IO b
+global f = do
   e <- newIORef ()
-  f (Event e)
+  f (Event (R $ EventIdRef e))
+
+local :: (Event a -> RSP b) -> RSP b
+local f = RSP $ liftF (Local f id)
 
 emit :: Event a -> a -> RSP ()
 emit e a = RSP $ liftF (Emit (ExEvent e a) ())
@@ -80,11 +97,17 @@ await :: Event a -> RSP a
 await e = RSP $ liftF (Await e id)
 
 orr' :: [RSP a] -> RSP (a, [RSP a])
-orr' rsps = undefined
+orr' [a] = (,[]) <$> a
+orr' [a, b] = second (:[]) <$> RSP (liftF (Or a b id))
+orr' (a:as) = do
+  r <- orr' [Left <$> a, Right <$> orr' as]
+  case r of
+    (Left x, _)        -> pure (x, as)
+    (Right (x, ks), _) -> pure (x, [a] <> ks)
 
 orr :: [RSP a] -> RSP a
 orr [a] = a
-orr [a, b] = RSP $ liftF (Or a b id)
+orr [a, b] = fmap fst $ RSP $ liftF (Or a b id)
 orr (a:as) = orr [a, orr as]
 
 andd :: [RSP a] -> RSP [a]
@@ -92,12 +115,7 @@ andd [a] = (:[]) <$> a
 andd [a, b] = RSP $ liftF (And a b id)
 andd (a:as) = concat <$> andd [(:[]) <$> a, andd as]
 
-forever :: RSP a
-forever = RSP $ liftF Forever
-
 --------------------------------------------------------------------------------
-
-data ExEvent = forall a. ExEvent (Event a) a
 
 bcast :: ExEvent -> RSP a -> RSP a
 bcast (ExEvent (Event e) a) (RSP (Free (Await (Event e') next)))
@@ -112,12 +130,12 @@ bcast e (RSP (Free (OrU p q next)))
 -- TODO
 bcast e (RSP (Free (Or p q next)))
   = RSP (Free (Or (bcast e p) (bcast e q) next))
+-- TODO
 bcast e (RSP (Free (Canrun n next)))
   = RSP (Free (Canrun n (getRSP $ bcast e (RSP next))))
 bcast _ p = p
 
-isBlocked :: RSP a -> Int -> Bool
-isBlocked (RSP (Pure _)) _ = True
+isBlocked :: RSP a -> StackLevel -> Bool
 isBlocked (RSP (Free (Await _ _))) _ = True
 isBlocked (RSP (Free (Canrun m _))) n = n > m
 isBlocked (RSP (Free (AndU p q _))) n = isBlocked p n && isBlocked q n
@@ -126,140 +144,100 @@ isBlocked _ _ = False
 
 step
   :: Maybe ExEvent
-  -> Int
+  -> StackLevel
+  -> EventIdInt
   -> RSP a
-  -> (Maybe ExEvent, Int, RSP a, IO ())
+  -> (Maybe ExEvent, StackLevel, EventIdInt, RSP a, IO ())
 
 -- push
-step (Just event) n p
-  = trace "push" (Nothing, n + 1, bcast event p, pure ())
+step (Just event) n eid p
+  = (Nothing, n + 1, eid + 1, bcast event p, pure ())
 -- pop
-step Nothing n p
-  | n > 0 && isBlocked p n = trace "pop" (Nothing, n - 1, p, pure ())
+step Nothing n eid p
+  | n > 0 && isBlocked p n = (Nothing, n - 1, eid + 1, p, pure ())
+
+-- local
+step Nothing n eid (RSP (Free (Local f next)))
+  = (Nothing, n, eid + 1, f (Event (I eid)) >>= RSP . next, pure ())
 
 -- emit
-step Nothing n (RSP (Free (Emit e next)))
-  = (Just e, n, RSP (Free (Canrun n next)), pure ())
+step Nothing n eid (RSP (Free (Emit e next)))
+  = (Just e, n, eid + 1, RSP (Free (Canrun n next)), pure ())
 
 -- canrun
-step Nothing n (RSP (Free (Canrun m next)))
-  | n == m = (Nothing, n, RSP next, pure ())
+step Nothing n eid (RSP (Free (Canrun m next)))
+  | n == m = (Nothing, n, eid + 1, RSP next, pure ())
 
 -- async
-step Nothing n (RSP (Free (Async io next)))
-  = (Nothing, n, RSP next, io)
+step Nothing n eid (RSP (Free (Async io next)))
+  = (Nothing, n, eid + 1, RSP next, io)
 
 -- and-expd
-step Nothing n (RSP (Free (And p (RSP q) next)))
-  = (Nothing, n, RSP (Free (AndU p (RSP (Free (Canrun n q))) next)), pure ())
+step Nothing n eid (RSP (Free (And p (RSP q) next)))
+  = (Nothing, n, eid + 1, RSP (Free (AndU p (RSP (Free (Canrun n q))) next)), pure ())
 -- and-nop1
-step Nothing n (RSP (Free (AndU (RSP (Pure a)) q next)))
-  = (Nothing, n, q' a, pure ())
+step Nothing n eid (RSP (Free (AndU (RSP (Pure a)) q next)))
+  = (Nothing, n, eid + 1, q' a, pure ())
   where
    q' a = do
      b <- q
      RSP (next [a, b])
 -- and-nop2
-step Nothing n (RSP (Free (AndU p (RSP (Pure b)) next)))
-  | isBlocked p n = (Nothing, n, p' b, pure ())
+step Nothing n eid (RSP (Free (AndU p (RSP (Pure b)) next)))
+  | isBlocked p n = (Nothing, n, eid + 1, p' b, pure ())
   where
     p' b = do
       a <- p
       RSP (next [a, b])
 -- and-adv2
-step Nothing n (RSP (Free (AndU p q next)))
+step Nothing n eid (RSP (Free (AndU p q next)))
   | isBlocked p n
-  && n == n' = (e, n, RSP (Free (AndU p q' next)), io)
+  && n == n' = (e, n, eid', RSP (Free (AndU p q' next)), io)
   where
-    (e, n', q', io) = step Nothing n q
+    (e, n', eid', q', io) = step Nothing n eid q
 -- and-adv1
-step Nothing n (RSP (Free (AndU p q next)))
-  | n == n' = (e, n, RSP (Free (AndU p' q next)), io)
+step Nothing n eid (RSP (Free (AndU p q next)))
+  | n == n' = (e, n, eid', RSP (Free (AndU p' q next)), io)
   where
-    (e, n', p', io) = step Nothing n p
+    (e, n', eid', p', io) = step Nothing n eid p
 
 -- or-expd
-step Nothing n (RSP (Free (Or p (RSP q) next)))
-  = (Nothing, n, RSP (Free (OrU p (RSP (Free (Canrun n q))) next)), pure ())
+step Nothing n eid (RSP (Free (Or p (RSP q) next)))
+  = (Nothing, n, eid + 1, RSP (Free (OrU p (RSP (Free (Canrun n q))) next)), pure ())
 -- or-nop1
-step Nothing n (RSP (Free (OrU (RSP (Pure a)) q next)))
-  = (Nothing, n, RSP (next a), pure ())
+step Nothing n eid (RSP (Free (OrU (RSP (Pure a)) q next)))
+  = (Nothing, n, eid + 1, RSP (next (a, q)), pure ())
 -- or-nop2
-step Nothing n (RSP (Free (OrU p (RSP (Pure b)) next)))
-  | isBlocked p n = (Nothing, n, RSP (next b), pure ())
+step Nothing n eid (RSP (Free (OrU p (RSP (Pure b)) next)))
+  | isBlocked p n = (Nothing, n, eid + 1, RSP (next (b, p)), pure ())
 -- or-adv2
-step Nothing n (RSP (Free (OrU p q next)))
+step Nothing n eid (RSP (Free (OrU p q next)))
   | isBlocked p n
-  && n == n' = (e, n, RSP (Free (OrU p q' next)), io)
+  && n == n' = (e, n, eid', RSP (Free (OrU p q' next)), io)
   where
-    (e, n', q', io) = step Nothing n q
+    (e, n', eid', q', io) = step Nothing n eid q
 -- or-adv1
-step Nothing n (RSP (Free (OrU p q next)))
-  | n == n' = (e, n, RSP (Free (OrU p' q next)), io)
+step Nothing n eid (RSP (Free (OrU p q next)))
+  | n == n' = (e, n, eid', RSP (Free (OrU p' q next)), io)
   where
-    (e, n', p', io) = step Nothing n p
+    (e, n', eid', p', io) = step Nothing n eid p
 
-step e n p = error (isEvent e <> ", " <> show n <> ", " <> show p)
+step e eid n p = error (isEvent e <> ", " <> show n <> ", " <> show eid <> ", " <> show p)
   where
-    isEvent (Just _) = "EVENT"
-    isEvent Nothing  = "NOEVENT"
+    isEvent (Just _) = "Event"
+    isEvent Nothing  = "No event"
 
 --------------------------------------------------------------------------------
 
-run e n p = do
-  traceIO (show n <> ", " <> show p)
-  let (e', n', p', io) = step e n p
-  io
-  case p' of
-    RSP (Pure a) -> pure a
-    _ -> run e' n' p'
-
-runST = run Nothing 0
-
-p1 = local $ \e -> runST $ do
-  a <- andd [ Left <$> ((,) <$> await e <*> await e), Right <$> emit e "A", Right <$> emit e "C" ]
-  async $ traceIO (show a)
-  a <- orr [ Left <$> await e, Right <$> emit e "B" ]
-  pure a
-
-p2 = local $ \e -> runST $ do
-  a <- andd [ Left <$> emit e "E", Right <$> await e ]
-  pure a
-
-p3 = local $ \e -> local $ \f -> runST $ do
-  a <- andd
-    [ Left  <$> (await e >> emit f "F")
-    , Right <$> await f
-    , Left  <$> emit e "E"
-    ]
-  pure a
-
-p4 = local $ \e -> local $ \f -> runST $do
-  a <- andd
-    [ Left  <$> andd [ Left <$> await e, Right <$> emit f "F" ]
-    , Right <$> await f
-    , Left  <$> andd [ Left <$> pure "_", Right <$> (await f >> emit e "E") ]
-    ]
-  pure a
-
-p5 = local $ \e -> runST $ do
-  andd
-    [ Left  <$> go 0 e
-    , Right <$> do
-        emit e (Left 1)
-        emit e (Left 2)
-        emit e (Left 3)
-        emit e (Left 4)
-        emit e (Right ())
-    ]
+run :: RSP a -> IO a
+run = go Nothing 0 0
   where
-    go :: Int -> Event (Either Int ()) -> RSP Int
-    go s e = do
-      a <- await e
-      async $ traceIO "BLA"
-      case a of
-        Left n  -> go (s + n) e
-        Right _ -> pure s
+    go e n eid p = do
+      let (e', n', eid', p', io) = step e n eid p
+      io
+      case p' of
+        RSP (Pure a) -> pure a
+        _ -> go e' n' eid' p'
 
 --------------------------------------------------------------------------------
 
@@ -286,41 +264,4 @@ p5 = local $ \e -> runST $ do
 -- with = undefined
 
 --------------------------------------------------------------------------------
-
--- testRSP :: RSP ()
--- testRSP = undefined
--- 
--- type Application a = (a -> IO Context) -> IO Context
--- 
--- server :: Application (Event String)
--- server app = do
---   e <- newEvent
---   ctx <- app e
---   emit ctx e "a"
---   emit ctx e "b"
---   pure ctx
--- 
--- m = server $ \x -> server $ \y -> server $ \z -> run $ do
---   a <- andd [ await x, await y, await z ]
---   async $ traceIO $ show a
---   a <- fst <$> orr [ await x, await y, await z ]
---   async $ traceIO a
--- 
--- m2 = server $ \x -> server $ \y -> server $ \z -> run $ do
---   a <- andd [ await x, await x, await x ]
---   async $ traceIO $ show a
---   a <- andd [ await x, await x, await x ]
---   async $ traceIO $ show a
--- 
--- boot :: Application (Event ())
--- boot app = do
---   boot <- newEvent
---   ctx <- app boot
---   emit ctx boot ()
---   pure ctx
--- 
--- m3 = boot $ \b -> run $ local $ \e -> do
---   await b
---   async $ traceIO "BOOTED"
---   a <- andd [ Left <$> awaitI e, Right <$> emitI e "asd" ]
---   async $ print a
+  
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,93 @@
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import RSP
+
+p1 = global $ \e -> run $ do
+  a <- andd [ Left <$> ((,) <$> await e <*> await e), Right <$> emit e "A", Right <$> emit e "C" ]
+  b <- orr [ Left <$> await e, Right <$> emit e "B" ]
+  pure (a, b)
+
+p2 = global $ \e -> run $ do
+  a <- andd [ Left <$> emit e "E", Right <$> await e ]
+  pure a
+
+p3 = global $ \e -> global $ \f -> run $ do
+  a <- andd
+    [ Left  <$> (await e >> emit f "F")
+    , Right <$> await f
+    , Left  <$> emit e "E"
+    ]
+  pure a
+
+p4 = global $ \e -> global $ \f -> run $do
+  a <- andd
+    [ Left  <$> andd [ Left <$> await e, Right <$> emit f "F" ]
+    , Right <$> await f
+    , Left  <$> andd [ Left <$> pure "_", Right <$> (await f >> emit e "E") ]
+    ]
+  pure a
+
+p5 = global $ \e -> run $ do
+  andd
+    [ Left  <$> go 0 e
+    , Right <$> do
+        emit e (Left 1)
+        emit e (Left 2)
+        emit e (Left 3)
+        emit e (Left 4)
+        emit e (Right ())
+    ]
+  where
+    go :: Int -> Event (Either Int ()) -> RSP Int
+    go s e = do
+      a <- await e
+      case a of
+        Left n  -> go (s + n) e
+        Right _ -> pure s
+
+p6 = run $ local $ \e -> local $ \f -> local $ \g -> do
+  a <- andd
+    [ Left  <$> andd [ Left <$> await e, Right <$> (emit f "F" >> await g >> emit e "E") ]
+    , Right <$> andd [ await f, await g, await e ]
+    , Right <$> andd [ await e, await g, await f ]
+    , Left  <$> andd [ Left <$> pure "_", Right <$> (await f >> emit g "G") ]
+    ]
+  pure a
+
+p7 = global $ \e -> global $ \f -> run $ do
+  andd
+    [ Left  <$> go 0 0 e f
+    , Right <$> do
+        emit e (Left 1)
+        emit e (Left 2)
+        emit e (Left 3)
+        emit e (Left 4)
+        emit f 9
+        emit f 1
+        emit e (Right ())
+    ]
+  where
+    go :: Int -> Int -> Event (Either Int ()) -> Event Int -> RSP Int
+    go x y e f = do
+      a <- orr [ Left <$> await e, Right <$> await f ]
+      case a of
+        Left (Left x') -> go (x + x') y e f
+        Right y'       -> go x (y + y') e f
+        _              -> pure (x + y)
+
+--------------------------------------------------------------------------------
+
+test :: (Show a, Eq a) => IO a -> a -> Assertion
+test f a = f >>= (@?= a)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain $ testGroup "Unit tests"
+  [ testCase "p1" $ test p1 ([Left ("A","C"),Right (),Right ()],Left "B")
+  , testCase "p2" $ test p2 [Left (),Right "E"]
+  , testCase "p3" $ test p3 [Left (),Right "F",Left ()]
+  , testCase "p4" $ test p4 [Left [Left "E",Right ()],Right "F",Left [Left "_",Right ()]]
+  , testCase "p5" $ test p5 [Left 10,Right ()]
+  , testCase "p6" $ test p6 [Left [Left "E",Right ()],Right ["F","G","E"],Right ["E","G","F"],Left [Left "_",Right ()]]
+  , testCase "p7" $ test p7 [Left 20,Right ()]
+  ]
